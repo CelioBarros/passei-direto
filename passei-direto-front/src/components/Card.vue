3818159c89195ebed39cf5aa9d294e5362c4e44a
Célio Barros <celio.barros.filho@gmail.com>
2019-09-10 01:35:48 -0300

    Create list view and card component

---
+++ passei-direto-front/src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <b-card>
+      <b-card-title>
+        Nome
+      </b-card-title>
+      <b-card-title>
+        Banda
+      </b-card-title>
+      <b-card-text>
+        Data
+      </b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card"
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  max-width: 15rem;
+}
+</style>
---
+++ passei-direto-front/src/views/Disk/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="list">
+    <h1>Lista de Discos</h1>
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card'
+
+export default {
+  name: 'DiskList',
+  components: {
+    Card,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
