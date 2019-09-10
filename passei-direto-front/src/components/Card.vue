5a099c9a6737e188e0fdce5fdd5e63110d3a8bfe
Célio Barros <celio.barros.filho@gmail.com>
2019-09-10 02:02:20 -0300

    Put valus on card list

--- passei-direto-front/src/components/Card.vue
+++ passei-direto-front/src/components/Card.vue
@@ -2,13 +2,13 @@
   <div class="card">
     <b-card>
       <b-card-title>
-        Nome
+        {{ name }}
       </b-card-title>
-      <b-card-title>
-        Banda
-      </b-card-title>
+      <b-card-sub-title>
+        {{ band }}
+      </b-card-sub-title>
       <b-card-text>
-        Data
+        {{ date }}
       </b-card-text>
     </b-card>
   </div>
@@ -16,12 +16,29 @@
 
 <script>
 export default {
-  name: "Card"
+  name: "Card",
+  props: {
+    name: {
+      type: String,
+      default: "",
+      required: true
+    },
+    band: {
+      type: String,
+      default: "",
+      required: true
+    },
+    date: {
+      type: String,
+      default: "00/00/0000",
+      required: true
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .card {
-  max-width: 15rem;
+  width: 12rem;
 }
 </style>
--- passei-direto-front/src/views/Disk/List.vue
+++ passei-direto-front/src/views/Disk/List.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="list">
     <h1>Lista de Discos</h1>
-    <Card />
+    <div class="d-flex flex-wrap justify-content-between">
+      <Card
+        class="mb-3"
+        v-for="(card, index) in cards"
+        :key="index"
+        :name="card.name"
+        :band="card.band"
+        :date="card.date"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+const cards = [
+  { name: "Disco 1", band: "Banda 1", date: "12/12/2012" },
+  { name: "Disco 2", band: "Banda 1", date: "12/12/2012" },
+  { name: "Disco 3", band: "Banda 2", date: "12/12/1211" },
+  { name: "Disco 4", band: "Banda 3", date: "12/12/2012" },
+  { name: "Disco 5", band: "Banda 4", date: "12/12/2012" },
+  { name: "Disco 6", band: "Banda 2", date: "12/12/2012" }
+];
 import Card from "@/components/Card";
 
 export default {
   name: "DiskList",
   components: {
     Card
+  },
+  data() {
+    return {
+      cards
+    };
   }
 };
 </script>
