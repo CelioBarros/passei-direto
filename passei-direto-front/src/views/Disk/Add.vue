11db683e91e596feca7280a799b8696c5b2352fb
Célio Barros <celio.barros.filho@gmail.com>
2019-09-10 23:49:58 -0300

    Finished add view

--- passei-direto-front/src/views/Disk/Add.vue
+++ passei-direto-front/src/views/Disk/Add.vue
@@ -1,16 +1,104 @@
 <template>
   <div>
-    <Modal :size="'md'" :id="'add-modal'" :title="'Adicionar Disco'"></Modal>
+    <b-modal
+      :id="modalId"
+      centered
+      scrollable
+      :size="'md'"
+      :title="'Adicionar Disco'"
+      @hidden="resetModal"
+      @ok="save"
+    >
+      <b-form-group @submit="save">
+        <b-form-group
+          id="input-group-1"
+          label="Nome do disco:"
+          label-for="input-1"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="form.name"
+            type="text"
+            placeholder="Nome do disco"
+            @blur="$v.form.name.$touch()"
+            :state="!$v.form.name.$error ? null : false"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="input-group-2"
+          label="Nome da banda:"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="form.band"
+            type="text"
+            placeholder="Nome da banda"
+            @blur="$v.form.band.$touch()"
+            :state="!$v.form.band.$error ? null : false"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="input-group-3"
+          label="Data de lançamento:"
+          label-for="input-3"
+        >
+          <b-form-input
+            id="input-3"
+            v-model="form.date"
+            type="date"
+            @blur="$v.form.date.$touch()"
+            :state="!$v.form.date.$error ? null : false"
+          >
+          </b-form-input>
+        </b-form-group>
+      </b-form-group>
+    </b-modal>
   </div>
 </template>
 
 <script>
-import Modal from "@/components/Modal";
+import { required } from "vuelidate/lib/validators";
 
 export default {
   name: "DiskList",
-  components: {
-    Modal
+  computed: {
+    modalId() {
+      return "add-modal";
+    }
+  },
+  data() {
+    return {
+      form: {
+        name: null,
+        band: null,
+        date: null
+      }
+    };
+  },
+  validations: {
+    form: {
+      name: {
+        required
+      },
+      band: {
+        required
+      },
+      date: {
+        required
+      }
+    }
+  },
+  methods: {
+    save() {},
+    resetModal() {
+      this.form.name = null;
+      this.form.band = null;
+      this.form.date = null;
+      this.$v.$reset();
+    }
   }
 };
 </script>
